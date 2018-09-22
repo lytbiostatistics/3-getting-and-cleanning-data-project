187a5e8fa0ed74db2acdbed6d422537b11e3244c
lytbiostatistics <42160887+lytbiostatistics@users.noreply.github.com>
2018-09-22 21:17:16 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,13 @@
-packages <- c("reshape2"，"data.table")
+# Load Packages and get the Data
+packages <- c("data.table", "reshape2")
 sapply(packages, require, character.only=TRUE, quietly=TRUE)
 path <- getwd()
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url, file.path(path, "dataFiles.zip"))
 unzip(zipfile = "dataFiles.zip")
+
+# Load activity labels + features
+
 activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt")
                         , col.names = c("classLabels", "activityName"))
 features <- fread(file.path(path, "UCI HAR Dataset/features.txt")
@@ -11,13 +15,19 @@ features <- fread(file.path(path, "UCI HAR Dataset/features.txt")
 featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
 measurements <- features[featuresWanted, featureNames]
 measurements <- gsub('[()]', '', measurements)
+
+# Load train datasets
+
 train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
 data.table::setnames(train, colnames(train), measurements)
 trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt")
-                         , col.names = c("Activity"))
+                       , col.names = c("Activity"))
 trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt")
                        , col.names = c("SubjectNum"))
 train <- cbind(trainSubjects, trainActivities, train)
+
+# Load test datasets
+
 test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
 data.table::setnames(test, colnames(test), measurements)
 testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
@@ -25,10 +35,16 @@ testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt")
                       , col.names = c("SubjectNum"))
 test <- cbind(testSubjects, testActivities, test)
+
+# merge datasets
+
 combined <- rbind(train, test)
+
+# Convert classLabels to activityName basically. More explicit. 
+
 combined[["Activity"]] <- factor(combined[, Activity]
-                                 , levels = activityLabels[["classLabels"]]
-                                 , labels = activityLabels[["activityName"]])
+                              , levels = activityLabels[["classLabels"]]
+                              , labels = activityLabels[["activityName"]])
 
 combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
 combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
